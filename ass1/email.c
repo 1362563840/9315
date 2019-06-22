b26d3390ab60d5f06319056fec180505e705695e
Athos <z5134298@ad.unsw.edu.au>
2019-06-22 22:50:20 +1000

    almost done. but index is not tested yet

--- ass1/email.c
+++ ass1/email.c
@@ -1,5 +1,5 @@
 /*
- * src/tutorial/complex.c
+ * src/tutorial/email.c
  *
  ******************************************************************************
   This file contains routines that can be bound to a Postgres backend and
@@ -316,6 +316,14 @@ email_in(PG_FUNCTION_ARGS)
   /**
    * shoud just be perfect exactly same space incluing '\0'
    */
+  int i = 0;
+  for( i = 0 ; i < strlen(local) + 1 ; i++ ) {
+    local[i] = tolower(local[i]);
+  }
+  i = 0;
+  for( i = 0 ; i < strlen(local) + 1 ; i++ ) {
+    domain[i] = tolower(domain[i]);
+  }
   strcpy(result -> local, local);
   strcpy(result -> domain, domain);
 
@@ -343,60 +351,13 @@ email_out(PG_FUNCTION_ARGS)
 	PG_RETURN_CSTRING(result);
 }
 
-// /*****************************************************************************
-//  * Binary Input/Output functions
-//  *
-//  * These are optional.
-//  *****************************************************************************/
 
-// PG_FUNCTION_INFO_V1(complex_recv);
+/*****************************************************************************
+ * New Operators
+ *
+ * A practical Complex datatype would provide much more than this, of course.
+ *****************************************************************************/
 
-// Datum
-// complex_recv(PG_FUNCTION_ARGS)
-// {
-// 	StringInfo	buf = (StringInfo) PG_GETARG_POINTER(0);
-// 	Complex    *result;
-
-// 	result = (Complex *) palloc(sizeof(Complex));
-// 	result->x = pq_getmsgfloat8(buf);
-// 	result->y = pq_getmsgfloat8(buf);
-// 	PG_RETURN_POINTER(result);
-// }
-
-// PG_FUNCTION_INFO_V1(complex_send);
-
-// Datum
-// complex_send(PG_FUNCTION_ARGS)
-// {
-// 	Complex    *complex = (Complex *) PG_GETARG_POINTER(0);
-// 	StringInfoData buf;
-
-// 	pq_begintypsend(&buf);
-// 	pq_sendfloat8(&buf, complex->x);
-// 	pq_sendfloat8(&buf, complex->y);
-// 	PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
-// }
-
-// /*****************************************************************************
-//  * New Operators
-//  *
-//  * A practical Complex datatype would provide much more than this, of course.
-//  *****************************************************************************/
-
-// PG_FUNCTION_INFO_V1(complex_add);
-
-// Datum
-// complex_add(PG_FUNCTION_ARGS)
-// {
-// 	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-// 	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
-// 	Complex    *result;
-
-// 	result = (Complex *) palloc(sizeof(Complex));
-// 	result->x = a->x + b->x;
-// 	result->y = a->y + b->y;
-// 	PG_RETURN_POINTER(result);
-// }
 
 
 // /*****************************************************************************
@@ -410,84 +371,102 @@ email_out(PG_FUNCTION_ARGS)
 //  * an internal three-way-comparison function, as we do here.
 //  *****************************************************************************/
 
-// #define Mag(c)	((c)->x*(c)->x + (c)->y*(c)->y)
 
-// static int
-// complex_abs_cmp_internal(Complex * a, Complex * b)
-// {
-// 	double		amag = Mag(a),
-// 				bmag = Mag(b);
+static int
+email_abs_cmp_internal(EmailAddr * a, EmailAddr * b)
+{
+  // diff in local part
+	if( strcmp( a -> local, b -> local ) < 0 ) {
+    return  -1;
+  }
+  if( strcmp( a -> local, b -> local ) > 0 ) {
+    return 1;
+  }
 
-// 	if (amag < bmag)
-// 		return -1;
-// 	if (amag > bmag)
-// 		return 1;
-// 	return 0;
-// }
+  // diff in domain part
+  if( strcmp( a -> domain, b -> domain ) < 0 ) {
+    return  -1;
+  }
+  if( strcmp( a -> domain, b -> domain ) > 0 ) {
+    return  1;
+  }
+  return 0;
+}
 
 
-// PG_FUNCTION_INFO_V1(complex_abs_lt);
+PG_FUNCTION_INFO_V1(email_abs_lt);
 
-// Datum
-// complex_abs_lt(PG_FUNCTION_ARGS)
-// {
-// 	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-// 	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+Datum
+email_abs_lt(PG_FUNCTION_ARGS)
+{
+	EmailAddr    *a = (EmailAddr *) PG_GETARG_POINTER(0);
+	EmailAddr    *b = (EmailAddr *) PG_GETARG_POINTER(1);
 
-// 	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) < 0);
-// }
+	PG_RETURN_BOOL(email_abs_cmp_internal(a, b) < 0);
+}
 
-// PG_FUNCTION_INFO_V1(complex_abs_le);
+PG_FUNCTION_INFO_V1(email_abs_le);
 
-// Datum
-// complex_abs_le(PG_FUNCTION_ARGS)
-// {
-// 	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-// 	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+Datum
+email_abs_le(PG_FUNCTION_ARGS)
+{
+	EmailAddr    *a = (EmailAddr *) PG_GETARG_POINTER(0);
+	EmailAddr    *b = (EmailAddr *) PG_GETARG_POINTER(1);
 
-// 	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) <= 0);
-// }
+	PG_RETURN_BOOL(email_abs_cmp_internal(a, b) <= 0);
+}
 
-// PG_FUNCTION_INFO_V1(complex_abs_eq);
+PG_FUNCTION_INFO_V1(email_abs_eq);
 
-// Datum
-// complex_abs_eq(PG_FUNCTION_ARGS)
-// {
-// 	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-// 	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+Datum
+email_abs_eq(PG_FUNCTION_ARGS)
+{
+	EmailAddr    *a = (EmailAddr *) PG_GETARG_POINTER(0);
+	EmailAddr    *b = (EmailAddr *) PG_GETARG_POINTER(1);
 
-// 	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) == 0);
-// }
+	PG_RETURN_BOOL(email_abs_cmp_internal(a, b) == 0);
+}
 
-// PG_FUNCTION_INFO_V1(complex_abs_ge);
+PG_FUNCTION_INFO_V1(email_abs_inequality);
 
-// Datum
-// complex_abs_ge(PG_FUNCTION_ARGS)
-// {
-// 	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-// 	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+Datum
+email_abs_inequality(PG_FUNCTION_ARGS)
+{
+	EmailAddr    *a = (EmailAddr *) PG_GETARG_POINTER(0);
+	EmailAddr    *b = (EmailAddr *) PG_GETARG_POINTER(1);
 
-// 	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) >= 0);
-// }
+	PG_RETURN_BOOL(email_abs_cmp_internal(a, b) != 0);
+}
 
-// PG_FUNCTION_INFO_V1(complex_abs_gt);
+PG_FUNCTION_INFO_V1(email_abs_ge);
 
-// Datum
-// complex_abs_gt(PG_FUNCTION_ARGS)
-// {
-// 	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-// 	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+Datum
+email_abs_ge(PG_FUNCTION_ARGS)
+{
+	EmailAddr    *a = (EmailAddr *) PG_GETARG_POINTER(0);
+	EmailAddr    *b = (EmailAddr *) PG_GETARG_POINTER(1);
 
-// 	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) > 0);
-// }
+	PG_RETURN_BOOL(email_abs_cmp_internal(a, b) >= 0);
+}
 
-// PG_FUNCTION_INFO_V1(complex_abs_cmp);
+PG_FUNCTION_INFO_V1(email_abs_gt);
 
-// Datum
-// complex_abs_cmp(PG_FUNCTION_ARGS)
-// {
-// 	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-// 	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+Datum
+email_abs_gt(PG_FUNCTION_ARGS)
+{
+	EmailAddr    *a = (EmailAddr *) PG_GETARG_POINTER(0);
+	EmailAddr    *b = (EmailAddr *) PG_GETARG_POINTER(1);
 
-// 	PG_RETURN_INT32(complex_abs_cmp_internal(a, b));
-// }
+	PG_RETURN_BOOL(email_abs_cmp_internal(a, b) > 0);
+}
+
+PG_FUNCTION_INFO_V1(email_abs_cmp);
+
+Datum
+email_abs_cmp(PG_FUNCTION_ARGS)
+{
+	EmailAddr    *a = (EmailAddr *) PG_GETARG_POINTER(0);
+	EmailAddr    *b = (EmailAddr *) PG_GETARG_POINTER(1);
+
+	PG_RETURN_INT32(email_abs_cmp_internal(a, b));
+}
